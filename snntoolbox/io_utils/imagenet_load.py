07fea463d936983f200b9a8a9025b42534233361
Yuhuang Hu <duguyue100@gmail.com>
2016-08-30 19:30:08 +0200

    add a ImageNet images sample script

--- snntoolbox/io_utils/imagenet_load.py
+++ snntoolbox/io_utils/imagenet_load.py
@@ -11,6 +11,9 @@ from __future__ import division, absolute_import
 from future import standard_library
 
 import os
+from os.path import join
+import shutil
+import random
 import numpy as np
 
 from keras.preprocessing.image import ImageDataGenerator
@@ -18,9 +21,55 @@ from keras.preprocessing.image import ImageDataGenerator
 standard_library.install_aliases()
 
 
-def get_imagenet(path, filename=None):
+def sample_imagenet(origin_path, target_path, num_samples=50):
+    """Randomly sample images from ImageNet Training Dataset.
+
+    This function ramdomly samples images from each class of ImageNet Training
+    Dataset for normalizing pretained ImageNet model.
+
+    In case you don't have to permission to the data folder, make sure
+    your are running this function with super user.
+
+    Parameters
+    ----------
+    origin_path : string
+        The path to training dataset
+    target_path : string
+        The path to target destination
     """
-    Load imagenet classification dataset.
+    if not os.path.isdir(origin_path):
+        raise ValueError("The source folder is not existed!")
+    if not os.path.isdir(target_path):
+        os.makedirs(target_path)
+        print ("[MESSAGE] WARNING! The target path is not existed, "
+               "The path is created automatically.")
+
+    print ("[MESSAGE] Start Copying.")
+    folder_list = [f for f in os.listdir(origin_path)
+                   if os.path.isdir(join(origin_path, f))]
+
+    for folder_name in folder_list:
+        if not os.path.isdir(join(target_path, folder_name)):
+            os.makedirs(join(target_path, folder_name))
+        file_list = [f for f in os.listdir(origin_path)
+                     if os.path.isfile(join(origin_path, f)) and ".JPEG" in f]
+
+        if num_samples > len(file_list):
+            file_idx = range(len(file_list))
+        else:
+            file_idx = random.sample(range(len(file_list)), num_samples)
+
+        for idx in file_idx:
+            shutil.copy(join(origin_path, folder_name, file_list[idx]),
+                        join(target_path, folder_name))
+            print ("[MESSAGE] Image %s is copied to %s" %
+                   (file_list[idx], join(target_path, folder_name)))
+
+    print ("[MESSAGE] Images are sampled! Stored at %s" % (target_path))
+
+
+def get_imagenet(path, filename=None):
+    """Load imagenet classification dataset.
 
     Values are normalized and saved as ``float32`` type. Class vectors are
     converted to binary class matrices. Output can be flattened for use in
@@ -46,7 +95,6 @@ def get_imagenet(path, filename=None):
     ``Y_test`` has dimension (num_samples, num_classes).
 
     """
-
     datagen = ImageDataGenerator()
     dataflow = datagen.flow_from_directory(path, target_size=(224, 224),
                                            batch_size=1000)
@@ -73,4 +121,6 @@ def get_imagenet(path, filename=None):
     np.savez_compressed(filepath + 'Y_test', y_test)
 
 if __name__ == '__main__':
-    get_imagenet('/home/rbodo/.snntoolbox/datasets/caltech101/original/')
+    # get_imagenet('/home/rbodo/.snntoolbox/datasets/caltech101/original/')
+    sample_imagenet("/home/duguyue100/imagenet/ILSVRC2015/Data/CLS-LOC/train/",
+                    "/home/duguyue100/imagenet_train/", num_samples=5)
