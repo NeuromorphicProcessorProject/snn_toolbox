Unified reset mechanism of membrane potentials across different activations functions. Fixed bug in parameter normalization such that scale factor of softmax is 1. Encapsulated submethods of DVSIterator to make them importable from other scripts.

snntoolbox/core/inisim.py
@@ -92,29 +92,7 @@ def update_payload(self, residuals, idxs):
 def linear_activation(self):
     """Linear activation."""
 
-    # Destroy impulse if in refractory period
-    masked_imp = self.impulse if settings['tau_refrac'] == 0 else \
-        t.set_subtensor(self.impulse[t.nonzero(self.refrac_until > self.time)],
-                        0.)
-
-    # Add impulse
-    if clamp_var:
-        # Experimental: Clamp the membrane potential to zero until the
-        # presynaptic neurons fire at their steady-state rates. This helps avoid
-        # a transient response.
-        new_mem = theano.ifelse.ifelse(
-            t.lt(t.mean(self.var), 1e-4) +
-            t.gt(self.time, settings['duration'] / 2),
-            self.mem + masked_imp, self.mem)
-    elif settings['filename_clamp_indices'] != '':
-        # Set clamp-duration by a specific delay from layer to layer.
-        new_mem = theano.ifelse.ifelse(t.lt(self.time, self.clamp_idx),
-                                       self.mem, self.mem + masked_imp)
-    elif v_clip:
-        # Clip membrane potential to [-2, 2] to prevent too strong accumulation.
-        new_mem = theano.tensor.clip(self.mem + masked_imp, -3, 3)
-    else:
-        new_mem = self.mem + masked_imp
+    new_mem = get_new_mem(self)
 
     # Store spiking
     output_spikes = t.mul(t.ge(new_mem, self.v_thresh), self.v_thresh)
@@ -170,12 +148,39 @@ def softmax_activation(self):
 
 
 def get_new_mem(self):
+    """
+
+    :param self: 
+    :type self: 
+    :return: 
+    :rtype: 
+    """
 
     # Destroy impulse if in refractory period
     masked_impulse = self.impulse if settings['tau_refrac'] == 0 else \
         t.set_subtensor(self.impulse[t.nonzero(self.refrac_until > self.time)],
                         0.)
-    return self.mem + masked_impulse
+
+    # Add impulse
+    if clamp_var:
+        # Experimental: Clamp the membrane potential to zero until the
+        # presynaptic neurons fire at their steady-state rates. This helps avoid
+        # a transient response.
+        new_mem = theano.ifelse.ifelse(
+            t.lt(t.mean(self.var), 1e-4) +
+            t.gt(self.time, settings['duration'] / 2),
+            self.mem + masked_impulse, self.mem)
+    elif settings['filename_clamp_indices'] != '':
+        # Set clamp-duration by a specific delay from layer to layer.
+        new_mem = theano.ifelse.ifelse(t.lt(self.time, self.clamp_idx),
+                                       self.mem, self.mem + masked_impulse)
+    elif v_clip:
+        # Clip membrane potential to [-2, 2] to prevent too strong accumulation.
+        new_mem = theano.tensor.clip(self.mem + masked_impulse, -3, 3)
+    else:
+        new_mem = self.mem + masked_impulse
+
+    return new_mem
 
 
 def set_reset_mem(self, mem, spikes):
@@ -362,7 +367,7 @@ def init_neurons(self, input_shape, tau_refrac=0.):
     if any({'spiketrains', 'spikerates', 'correlation', 'spikecounts',
             'hist_spikerates_activations', 'operations', 'operations_b_t',
             'spiketrains_n_b_l_t'}
-                   & (settings['plot_vars'] | settings['log_vars'])):
+            & (settings['plot_vars'] | settings['log_vars'])):
         self.spiketrain = k.zeros(output_shape)
     if settings['online_normalization']:
         self.spikecounts = k.zeros(output_shape)
@@ -438,6 +443,9 @@ def update_b(self):
 class SpikeConcatenate(Concatenate):
     """Spike merge layer"""
 
+    def _merge_function(self, inputs):
+        return self._merge_function(inputs)
+
     @staticmethod
     def reset(sample_idx):
         """Reset layer variables."""
@@ -488,7 +496,6 @@ class SpikeDense(Dense):
         self.tau_refrac = kwargs['tau_refrac'] if 'tau_refrac' in kwargs else 0.
         self.v_thresh = None
         self.stateful = True
-        self.updates = []
         self._per_input_updates = {}
         self.time = None
         self.mem = self.spiketrain = self.impulse = self.spikecounts = None
@@ -565,7 +572,6 @@ class SpikeConv2D(Conv2D):
         self.tau_refrac = kwargs['tau_refrac'] if 'tau_refrac' in kwargs else 0.
         self.v_thresh = None
         self.stateful = True
-        self.updates = []
         self._per_input_updates = {}
         self.time = None
         self.mem = self.spiketrain = self.impulse = self.spikecounts = None
@@ -637,7 +643,6 @@ class SpikeAveragePooling2D(AveragePooling2D):
         self.tau_refrac = kwargs['tau_refrac'] if 'tau_refrac' in kwargs else 0.
         self.v_thresh = None
         self.stateful = True
-        self.updates = []
         self._per_input_updates = {}
         self.time = None
         self.mem = self.spiketrain = self.impulse = self.spikecounts = None
@@ -703,7 +708,6 @@ class SpikeMaxPooling2D(MaxPooling2D):
         self.tau_refrac = kwargs['tau_refrac'] if 'tau_refrac' in kwargs else 0.
         self.v_thresh = None
         self.stateful = True
-        self.updates = []
         self._per_input_updates = {}
         self.spikerate_pre = self.time = self.previous_x = None
         self.mem = self.spiketrain = self.impulse = self.spikecounts = None
@@ -1094,10 +1098,10 @@ class SpikePool(theano.Op):
                     # index 0 if all rates are equally zero.
                     continue
                 spike_patch = ysn[[region_slices[i][r[i]] for i in range(nd)]]
-                # The first condition is not completely equivalent to the second
-                # because the latter has a higher chance of admitting spikes.
+                # The second condition is not completely equivalent to the first
+                # because the former has a higher chance of admitting spikes.
+                # if (spike_patch*(rate_patch == np.argmax(rate_patch))).any():
                 if spike_patch.flatten()[np.argmax(rate_patch)]:
-                #if (spike_patch * (rate_patch == np.argmax(rate_patch))).any():
                     zzn[r] = spike
 
 

snntoolbox/core/util.py
@@ -19,7 +19,7 @@ from importlib import import_module
 import numpy as np
 from future import standard_library
 import keras
-from keras import backend as k
+from keras import backend
 from snntoolbox.config import settings
 
 standard_library.install_aliases()
@@ -55,7 +55,7 @@ def binary_tanh(x):
         Integer in {-1, 1}
     """
 
-    return k.sign(x)
+    return backend.sign(x)
 
 
 def binary_sigmoid(x):
@@ -73,9 +73,9 @@ def binary_sigmoid(x):
         Integer in {0, 1}
     """
 
-    x = k.clip((x + 1.) / 2., 0, 1)
+    x = backend.clip((x + 1.) / 2., 0, 1)
 
-    return k.round(x)
+    return backend.round(x)
 
 
 def parse(input_model):
@@ -472,17 +472,7 @@ def normalize_parameters(model, **kwargs):
             nonzero_activations = activations[np.nonzero(activations)]
             del activations
             idx = i if settings['normalization_schedule'] else 0
-            if layer.activation.__name__ == 'softmax':
-                # When using a certain percentile or even the max, the scaling
-                # factor can be extremely low in case of many output classes
-                # (e.g. 0.01 for ImageNet). This amplifies weights and biases
-                # greatly. But large biases cause large offsets in the beginning
-                # of the simulation (spike input absent).
-                sf = 1.0  # float(np.max(nonzero_activations))
-                print("Scale factor: {:.2f}.".format(sf))
-            else:
-                sf = get_scale_fac(nonzero_activations, idx)
-            scale_facs[layer.name] = sf
+            scale_facs[layer.name] = get_scale_fac(nonzero_activations, idx)
             # Since we have calculated output activations here, check at this
             # point if the output is mostly negative, in which case we should
             # stick to softmax. Otherwise ReLU is preferred.
@@ -513,7 +503,17 @@ def normalize_parameters(model, **kwargs):
 
         # Scale parameters
         parameters = layer.get_weights()
-        scale_fac = scale_facs[layer.name]
+        if layer.activation.__name__ == 'softmax':
+            # When using a certain percentile or even the max, the scaling
+            # factor can be extremely low in case of many output classes
+            # (e.g. 0.01 for ImageNet). This amplifies weights and biases
+            # greatly. But large biases cause large offsets in the beginning
+            # of the simulation (spike input absent).
+            scale_fac = 1.0
+            print("Using scale factor {:.2f} for softmax layer.".format(
+                scale_fac))
+        else:
+            scale_fac = scale_facs[layer.name]
         inbound = get_inbound_layers_with_params(layer)
         if len(inbound) == 0:  # Input layer
             input_layer = layer.inbound_nodes[0].inbound_layers[0].name

snntoolbox/io_utils/AedatTools/DVSIterator.py
@@ -7,6 +7,7 @@ for use in a time-stepped simulator.
 
 import os
 import numpy as np
+from snntoolbox.io_utils.common import to_categorical
 
 
 class DVSIterator(object):
@@ -68,9 +69,6 @@ class DVSIterator(object):
             self.num_samples, self.num_classes))
 
     def __next__(self):
-        from snntoolbox.io_utils.common import to_categorical
-        from collections import deque
-
         while self.num_events_per_batch * (self.batch_idx + 1) >= \
                 self.num_events_of_sample:
             self.dvs_sample_idx += 1
@@ -86,50 +84,82 @@ class DVSIterator(object):
             print("Number of batches: {:d}.".format(
                 int(self.num_events_of_sample / self.num_events_per_batch)))
 
-        print("Extracting batch of samples à {} events from DVS sequence..."
-              "".format(self.num_events_per_sample))
-        x_b_xaddr = [deque() for _ in range(self.batch_size)]
-        x_b_yaddr = [deque() for _ in range(self.batch_size)]
-        x_b_ts = [deque() for _ in range(self.batch_size)]
-        for sample_idx in range(self.batch_size):
-            start_event = self.num_events_per_batch * self.batch_idx + \
-                          self.num_events_per_sample * sample_idx
-            event_idxs = range(start_event,
-                               start_event + self.num_events_per_sample)
-            event_sums = np.zeros((64, 64), 'int32')
-            xaddr_sub = []
-            yaddr_sub = []
-            for x, y in zip(self.dvs_sample[0][event_idxs],
-                            self.dvs_sample[1][event_idxs]):
-                if self.scale:
-                    # Subsample from 240x180 to e.g. 64x64
-                    x = int(x / self.scale[0])
-                    y = int(y / self.scale[1])
-                event_sums[y, x] += 1
-                xaddr_sub.append(x)
-                yaddr_sub.append(y)
-            sigma = np.std(event_sums[np.nonzero(event_sums)])
-            # Clip number of events per pixel to three-sigma
-            np.clip(event_sums, 0, 3*sigma, event_sums)
-            print("Discarded {} events during 3-sigma standardization.".format(
-                self.num_events_per_sample - np.sum(event_sums)))
-            ts_sample = self.dvs_sample[2][event_idxs]
-            for x, y, ts in zip(xaddr_sub, yaddr_sub, ts_sample):
-                if event_sums[y, x] > 0:
-                    x_b_xaddr[sample_idx].append(x)
-                    x_b_yaddr[sample_idx].append(y)
-                    x_b_ts[sample_idx].append(ts)
-                    event_sums[y, x] -= 1
+        batch_idx = self.batch_idx
+        self.batch_idx += 1
+
+        xaddr_b, yaddr_b, ts_b = extract_batch(
+            self.dvs_sample[0], self.dvs_sample[1], self.dvs_sample[2],
+            self.batch_size, batch_idx, self.num_events_per_sample, self.scale)
 
         # Each sample in the batch has the same label because it is generated
         # from the same DVS sequence.
-        y_b = np.broadcast_to(to_categorical(
+        truth_b = np.broadcast_to(to_categorical(
             [self.labels[self.dvs_sample_idx]], self.num_classes),
             (self.batch_size, self.num_classes))
 
-        self.batch_idx += 1
+        return xaddr_b, yaddr_b, ts_b, truth_b
 
-        return x_b_xaddr, x_b_yaddr, x_b_ts, y_b
+
+def extract_batch(xaddr, yaddr, timestamps, batch_size, batch_idx=0,
+                  num_events_per_sample=2000, scale=(239/63, 179/63)):
+    """
+    Transform a one-dimensional sequence of AER-events into a batch.
+
+    :param xaddr: 
+    :type xaddr: 
+    :param yaddr: 
+    :type yaddr: 
+    :param timestamps: 
+    :type timestamps: 
+    :param batch_size: 
+    :type batch_size: 
+    :param batch_idx: 
+    :type batch_idx: 
+    :param num_events_per_sample: 
+    :type num_events_per_sample: 
+    :param scale: 
+    :type scale: 
+    :return: 
+    :rtype: 
+    """
+
+    from collections import deque
+
+    print("Extracting batch of samples à {} events from DVS sequence..."
+          "".format(num_events_per_sample))
+
+    x_b_xaddr = [deque() for _ in range(batch_size)]
+    x_b_yaddr = [deque() for _ in range(batch_size)]
+    x_b_ts = [deque() for _ in range(batch_size)]
+    for sample_idx in range(batch_size):
+        start_event = num_events_per_sample * batch_size * batch_idx + \
+                      num_events_per_sample * sample_idx
+        event_idxs = range(start_event, start_event + num_events_per_sample)
+        event_sums = np.zeros((64, 64), 'int32')
+        xaddr_sub = []
+        yaddr_sub = []
+        for x, y in zip(xaddr[event_idxs], yaddr[event_idxs]):
+            if scale:
+                # Subsample from 240x180 to e.g. 64x64
+                x = int(x / scale[0])
+                y = int(y / scale[1])
+            event_sums[y, x] += 1
+            xaddr_sub.append(x)
+            yaddr_sub.append(y)
+        sigma = np.std(event_sums[np.nonzero(event_sums)])
+        # Clip number of events per pixel to three-sigma
+        np.clip(event_sums, 0, 3*sigma, event_sums)
+        print("Discarded {} events during 3-sigma standardization.".format(
+            num_events_per_sample - np.sum(event_sums)))
+        ts_sample = timestamps[event_idxs]
+        for x, y, ts in zip(xaddr_sub, yaddr_sub, ts_sample):
+            if event_sums[y, x] > 0:
+                x_b_xaddr[sample_idx].append(x)
+                x_b_yaddr[sample_idx].append(y)
+                x_b_ts[sample_idx].append(ts)
+                event_sums[y, x] -= 1
+
+    return x_b_xaddr, x_b_yaddr, x_b_ts
 
 
 def remove_outliers(timestamps, xaddr, yaddr, pol, x_max=239, y_max=179):
@@ -165,15 +195,27 @@ def remove_outliers(timestamps, xaddr, yaddr, pol, x_max=239, y_max=179):
 
 def load_dvs_sequence(filename, xyrange=None):
     """
+    Load a sequence of AER-events from an ``.aedat`` file and return three
+    arrays containing the x, y addresses and the timestamps.
+    If an ``xyrange`` is given, events outside this range are removed. 
 
     Parameters
     ----------
 
-    filename:
-    xyrange:
+    filename: str
+        Name of ``.aedat`` file to load.
+    xyrange: tuple[int]
+        Chip dimensions - 1, i.e. largest indices with zero-convention. 
 
     Returns
     -------
+
+    xaddr: np.array
+        The x-addresses.
+    yaddr: np.array
+        The y-addresses.
+    timestamps: np.array
+        The timestamps.
 
     """
 