0dc823ce4737d27e3bb3c1329f0b3e02bbd71933
rbodo <bodo.rueckauer@gmail.com>
2018-02-02 15:33:05 +0100

    Updated experiment scripts with DVS data.

--- scripts/ann_architectures/imagenet/train_vgg16.py
+++ scripts/ann_architectures/imagenet/train_vgg16.py
@@ -6,37 +6,43 @@ from keras.applications.imagenet_utils import preprocess_input
 from keras.applications.vgg16 import VGG16
 from keras.callbacks import TensorBoard
 from keras.metrics import top_k_categorical_accuracy
+from keras.optimizers import SGD
 
 data_path = '/home/rbodo/.snntoolbox/Datasets/imagenet'
 train_path = os.path.join(data_path, 'training')
 test_path = os.path.join(data_path, 'validation')
 class_idx_path = os.path.join(data_path, 'imagenet_class_index_1000.json')
-log_path = '/home/rbodo/.snntoolbox/data/imagenet/vgg16_trained'
+log_path = '/home/rbodo/.snntoolbox/data/imagenet/vgg16_trained2'
+
+num_classes = 100
 
 print("Instantiating model...")
-model = VGG16(weights=None)
+model = VGG16(weights=None, classes=num_classes)
 
-model.compile('rmsprop', 'categorical_crossentropy',
-              ['accuracy', top_k_categorical_accuracy])
+sgd = SGD(0.01)
+model.compile(sgd, 'categorical_crossentropy',
+              ['accuracy'])  # , top_k_categorical_accuracy])
 
 # Get dataset
 print("Loading dataset...")
 class_idx = json.load(open(class_idx_path, "r"))
 classes = [class_idx[str(idx)][0] for idx in range(len(class_idx))]
+classes = classes[:num_classes]
 
 target_size = (224, 224)
-batch_size = 32
-nb_train_samples = 100000  # 1281167
+batch_size = 1
+nb_train_samples = 129395  # 1281167
 nb_train_steps = nb_train_samples / batch_size
-nb_val_samples = 10000  # 50000
+nb_val_samples = 50000  # 50000
 nb_val_steps = nb_val_samples / batch_size
-nb_epoch = 2
+nb_epoch = 10
 
 datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 trainflow = datagen.flow_from_directory(
     train_path, target_size, classes=classes, batch_size=batch_size)
 testflow = datagen.flow_from_directory(
-    test_path, target_size, classes=classes, batch_size=batch_size)
+    test_path, target_size, classes=classes, batch_size=nb_val_samples)
+testdata = testflow.next()
 
 # print("Evaluating initial model...")
 # score = model.evaluate_generator(testflow, nb_val_steps)
@@ -44,7 +50,8 @@ testflow = datagen.flow_from_directory(
 # 0.15% and 0.54%
 
 print("Training...")
-gradients = TensorBoard(log_path + '/logs', write_grads=True)
+gradients = TensorBoard(log_path + '/logs_32bit', 1, write_grads=True)
 model.fit_generator(trainflow, nb_train_steps, nb_epoch, verbose=1,
-                    callbacks=[gradients], validation_data=testflow,
-                    validation_steps=nb_val_steps)
+                    validation_data=testdata, callbacks=[gradients])
+
+model.save(os.path.join(log_path, 'vgg16.h5'))
---
+++ scripts/predator_prey/train_predator_prey.py
@@ -0,0 +1,68 @@
+# coding=utf-8
+
+"""Train a small CNN using frames generated from the DVS predator-prey data set.
+"""
+
+import os
+import numpy as np
+import keras
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.callbacks import ModelCheckpoint, TensorBoard
+
+data_path_base = '/home/rbodo/.snntoolbox/Datasets/predator_prey/npz/dvs/' \
+                 'rectified_sum'
+model_path_base = '/home/rbodo/.snntoolbox/data/predator_prey'
+
+activity_regularizer1 = keras.regularizers.l2(0.0001)
+activity_regularizer2 = keras.regularizers.l2(0.001)
+kernel_regularizer = keras.regularizers.l2(0.001)
+use_bias = True
+
+# Best settings
+data_path = os.path.join(data_path_base, 'maxpool_subsampling')
+log_path = os.path.join(model_path_base, 'logs', 'bias')
+
+# No maxpool_subsampling
+# data_path = os.path.join(data_path_base, '')
+# log_path = os.path.join(model_path_base, 'logs', 'sum_subsampling')
+
+# No 3-sigma standardization
+# data_path = os.path.join(data_path_base, 'maxpool_subsampling', 'no_clip')
+# log_path = os.path.join(model_path_base, 'logs', 'no_clip')
+
+# Scaling before 3-sigma standardization
+# data_path = os.path.join(data_path_base, 'maxpool_subsampling',
+#                          'scale_before_clip')
+# log_path = os.path.join(model_path_base, 'logs', 'scale_before_clip')
+
+x_train = np.load(os.path.join(data_path, 'x_train.npz'))['arr_0']
+x_test = np.load(os.path.join(data_path, 'x_test.npz'))['arr_0']
+y_train = np.load(os.path.join(data_path, 'y_train.npz'))['arr_0']
+y_test = np.load(os.path.join(data_path, 'y_test.npz'))['arr_0']
+
+model = keras.models.Sequential()
+model.add(Conv2D(4, (5, 5), input_shape=(36, 36, 1), use_bias=use_bias,
+                 activation='relu', activity_regularizer=None,
+                 kernel_regularizer=kernel_regularizer,
+                 bias_regularizer=kernel_regularizer))
+model.add(MaxPooling2D())
+model.add(Conv2D(4, (5, 5), use_bias=use_bias, activation='relu',
+                 activity_regularizer=activity_regularizer1,
+                 kernel_regularizer=kernel_regularizer,
+                 bias_regularizer=kernel_regularizer))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(40, use_bias=use_bias, activation='relu',
+                activity_regularizer=activity_regularizer2,
+                kernel_regularizer=kernel_regularizer,
+                bias_regularizer=kernel_regularizer))
+model.add(Dense(4, use_bias=use_bias, activation='softmax',
+                kernel_regularizer=kernel_regularizer,
+                bias_regularizer=kernel_regularizer))
+
+model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+
+checkpoint = ModelCheckpoint(os.path.join(
+    log_path, '{epoch:02d}-{val_acc:.2f}.h5'), 'val_acc', True)
+model.fit(x_train, y_train, epochs=30, validation_data=(x_test, y_test),
+          callbacks=[checkpoint, TensorBoard(log_path, 1)])
