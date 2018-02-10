561edb828b4706720265acdc32319f4c7191d2b6
rbodo <bodo.rueckauer@gmail.com>
2018-02-10 18:04:16 +0100

    More experiments on DVS input

--- scripts/predator_prey/train_predator_prey.py
+++ scripts/predator_prey/train_predator_prey.py
@@ -6,12 +6,14 @@
 import os
 import numpy as np
 import keras
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, GaussianNoise
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
 data_path_base = '/home/rbodo/.snntoolbox/Datasets/predator_prey/npz/dvs/' \
                  'rectified_sum'
 model_path_base = '/home/rbodo/.snntoolbox/data/predator_prey'
+
+kwargs = {'input_shape': (36, 36, 1)}
 
 activity_regularizer1 = keras.regularizers.l2(0.0001)
 activity_regularizer2 = keras.regularizers.l2(0.001)
@@ -19,8 +21,8 @@ kernel_regularizer = keras.regularizers.l2(0.001)
 use_bias = True
 
 # Best settings
-data_path = os.path.join(data_path_base, 'maxpool_subsampling')
-log_path = os.path.join(model_path_base, 'logs', 'bias')
+# data_path = os.path.join(data_path_base, 'maxpool_subsampling')
+# log_path = os.path.join(model_path_base, 'logs', 'bias')
 
 # No maxpool_subsampling
 # data_path = os.path.join(data_path_base, '')
@@ -35,16 +37,24 @@ log_path = os.path.join(model_path_base, 'logs', 'bias')
 #                          'scale_before_clip')
 # log_path = os.path.join(model_path_base, 'logs', 'scale_before_clip')
 
+# Additive Gaussian noise
+data_path = os.path.join(data_path_base, 'maxpool_subsampling')
+log_path = os.path.join(model_path_base, 'logs', 'gaussian_noise2')
+gaussian_noise = True
+
 x_train = np.load(os.path.join(data_path, 'x_train.npz'))['arr_0']
 x_test = np.load(os.path.join(data_path, 'x_test.npz'))['arr_0']
 y_train = np.load(os.path.join(data_path, 'y_train.npz'))['arr_0']
 y_test = np.load(os.path.join(data_path, 'y_test.npz'))['arr_0']
 
 model = keras.models.Sequential()
-model.add(Conv2D(4, (5, 5), input_shape=(36, 36, 1), use_bias=use_bias,
-                 activation='relu', activity_regularizer=None,
+if gaussian_noise:
+    model.add(GaussianNoise(0.0001, **kwargs))
+    kwargs = {}
+model.add(Conv2D(4, (5, 5), use_bias=use_bias, activation='relu',
+                 activity_regularizer=None,
                  kernel_regularizer=kernel_regularizer,
-                 bias_regularizer=kernel_regularizer))
+                 bias_regularizer=kernel_regularizer, **kwargs))
 model.add(MaxPooling2D())
 model.add(Conv2D(4, (5, 5), use_bias=use_bias, activation='relu',
                  activity_regularizer=activity_regularizer1,
