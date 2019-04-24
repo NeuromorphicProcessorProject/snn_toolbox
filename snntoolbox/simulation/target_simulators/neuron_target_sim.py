d49a2946498454733d1cadd1159f5fe51c9b9d7f
Bodo Rueckauer <bodo.rueckauer@gmail.com>
2019-04-24 11:33:17 -0700

    Change "Neuron" to "neuron" for pyNN simulator backend.

--- snntoolbox/simulation/target_simulators/neuron_target_sim.py
+++ snntoolbox/simulation/target_simulators/neuron_target_sim.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """Building and simulating spiking neural networks using Neuron.
 
-All the work is done in pyNN, so we simply redirect :py:mod:`Neuron_target_sim`
+All the work is done in pyNN, so we simply redirect :py:mod:`neuron_target_sim`
 to :py:mod:`pyNN_target_sim` here.
 
 @author: rbodo
