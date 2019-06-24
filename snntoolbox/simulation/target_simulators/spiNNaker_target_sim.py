18f0b814594a2061ba5dc71ea6f9f230e235a3c6
ej159 <edwardgeorgejones@gmail.com>
2019-06-24 11:25:18 +0100

    reverted changes to v_rest in input layers and commented out interception of simulator

--- snntoolbox/simulation/target_simulators/spiNNaker_target_sim.py
+++ snntoolbox/simulation/target_simulators/spiNNaker_target_sim.py
@@ -379,11 +379,11 @@ class SNN(PYSNN):
                  for amplitude in x_flat]
             self.layers[0].set(spike_times=spike_times)
 
-        from pynn_object_serialisation.functions import intercept_simulator
-        import pylab
-        current_time = pylab.datetime.datetime.now().strftime("_%H%M%S_%d%m%Y")
-        intercept_simulator(self.sim, "snn_toolbox_spinnaker_" + current_time,
-                            post_abort=False)
+        #from pynn_object_serialisation.functions import intercept_simulator
+        #import pylab
+        #current_time = pylab.datetime.datetime.now().strftime("_%H%M%S_%d%m%Y")
+        #intercept_simulator(self.sim, "snn_toolbox_spinnaker_" + current_time,
+        #                    post_abort=False)
         self.sim.run(self._duration - self._dt)
         print("\nCollecting results...")
         output_b_l_t = self.get_recorded_vars(self.layers)
