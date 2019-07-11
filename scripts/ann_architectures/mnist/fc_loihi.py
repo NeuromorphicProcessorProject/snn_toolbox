e6475ab4e069bd71d9f79f515d371c80b5699b88
brueckau <bodo.rueckauer@intel.com>
2019-07-11 12:21:24 -0700

    Modified experiments.

--- scripts/ann_architectures/mnist/fc_loihi.py
+++ scripts/ann_architectures/mnist/fc_loihi.py
@@ -9,14 +9,11 @@ from __future__ import print_function
 
 from keras.callbacks import ModelCheckpoint
 from keras.datasets import mnist as dataset
-from keras.layers import BatchNormalization, Activation, AveragePooling2D
-from keras.layers import Dense, Dropout, Flatten, Conv2D
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils import to_categorical
 
 from snntoolbox.simulation.plotting import plot_history
-import matplotlib.pyplot as plt
 from skimage.measure import block_reduce
 import numpy as np
 
@@ -33,8 +30,15 @@ subsample_shift = 4
 x_train = block_reduce(x_train, (1, subsample_shift, subsample_shift), np.mean)
 x_test = block_reduce(x_test, (1, subsample_shift, subsample_shift), np.mean)
 
+x_train /= 255.
+x_test /= 255.
+
 x_train = x_train.reshape((len(x_train), -1))
 x_test = x_test.reshape((len(x_test), -1))
+
+path = '/home/brueckau/Downloads/'
+np.savez_compressed(path + 'x_test', x_test)
+np.savez_compressed(path + 'x_train', x_train)
 
 model = Sequential()
 
@@ -46,7 +50,7 @@ model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
 
-checkpointer = ModelCheckpoint(filepath='fc.{epoch:02d}-{val_acc:.2f}.h5',
+checkpointer = ModelCheckpoint(filepath=path+'fc.{epoch:02d}-{val_acc:.2f}.h5',
                                verbose=1, save_best_only=True)
 
 history = model.fit(x_train, y_train, batch_size, nb_epoch,
