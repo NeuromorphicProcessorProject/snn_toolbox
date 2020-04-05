Bugfix in input layer. Improved weight settings in case that threshold normalization is turned off.

snntoolbox/simulation/plotting.py
@@ -14,6 +14,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from future import standard_library
 
+from snntoolbox.utils.utils import get_pearson_coefficients
+
 standard_library.install_aliases()
 
 
@@ -491,49 +493,6 @@ def plot_network_correlations(spikerates, layer_activations):
     f.text(0.5, 0.04, 'SNN spikerates (Hz)', ha='center', fontsize=16)
     f.text(0.04, 0.5, 'ANN activations', va='center', rotation='vertical',
            fontsize=16)
-
-
-def get_pearson_coefficients(spikerates_batch, activations_batch, max_rate):
-    """
-    Compute Pearson coefficients.
-
-    Parameters
-    ----------
-
-    spikerates_batch : 
-    activations_batch :
-    max_rate: float
-        Highest spike rate.
-
-    Returns
-    -------
-    
-    co: list
-
-    """
-
-    co = []
-    for layer_num in range(len(spikerates_batch)):
-        c = []
-        for sample in range(len(spikerates_batch[0][0])):
-            s = spikerates_batch[layer_num][0][sample].flatten()
-            a = activations_batch[layer_num][0][sample].flatten()
-            if layer_num < len(spikerates_batch) - 1:
-                # Remove points at origin and saturated units, except for
-                # output layer (has too few units and gets activation of 1
-                # because of softmax).
-                ss = []
-                aa = []
-                for sss, aaa in zip(s, a):
-                    if (sss > 0 or aaa > 0) and aaa < max_rate:
-                        ss.append(sss)
-                        aa.append(aaa)
-                s = ss
-                a = aa
-            c.append(np.corrcoef(s, a)[0, 1])
-        co.append(c)
-
-    return co
 
 
 def plot_pearson_coefficients(spikerates_batch, activations_batch, config,
@@ -1156,6 +1115,7 @@ def plot_input_image(x, label, path=None, data_format=None, filename=None):
 
     if x.ndim > 2:
         x = np.squeeze(x)
+        x = np.atleast_2d(x)
 
     plt.figure()
     plt.title('Input image (class: {})'.format(label))